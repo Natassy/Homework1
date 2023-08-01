27ce63ffa283a7a140e98d232394782c531a865b
Наталья Кужельная <134339049+Natassy@users.noreply.github.com>
2023-08-02 00:48:47 +0300

    Add files via upload

---
+++ lesson_1_task_1.py
@@ -0,0 +1,2 @@
+name="Наталья"
+print (name)
---
+++ lesson_1_task_7.py
@@ -0,0 +1,29 @@
+def number1():
+    return 1
+def number2():
+    return 2
+def number3():
+    return 3
+def number4():
+    return 4
+def number5():
+    return 5
+def number6():
+    return 6
+def number7():
+    return 7
+def number8():
+    return 8
+def number9():
+    return 9
+def number0():
+    return 0
+number8()
+x=number8()
+number0()
+a=number0()
+number5()
+b=number5()
+number3()
+c=number3()
+print(x,x,a,a,b,b,b,c,b,c,b, sep="")
